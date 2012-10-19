34528bd48b67dd03823f3d5578867bc6d1904b75
Massimo Lauria <lauria.massimo@gmail.com>
2012-10-19 14:33:36 +0200

    First commit

---
+++ cnfgen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+AUTHOR: Massimo Lauria <lauria@kth.se>
+COPYRIGHT: Massimo Lauria, 2012
+
+The module contains facilities to generate cnf formulas in dimacs
+format. Such formulas are ready to be fed to sat solvers.
+
+"""
+
+
+
+
+if __name__ == '__main__':
+    # Parse the command line arguments
+
+    # Select the appropriate generator
+
+    # Output the formula
+    print "Work in progress"
