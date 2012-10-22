b05bc5046ffde2d57916ecaa8ff0854f8400accf
Massimo Lauria <lauria.massimo@gmail.com>
2012-10-22 21:47:39 +0200

    Support for LaTeX output, and implementation of some formulas.
    In  particular RamseyNumber,OrderingPrinciple  and PigeonholePrinciple
    formulas.

--- cnfgen.py
+++ cnfgen.py
@@ -1,24 +1,55 @@
 #!/usr/bin/env python
-
 """
-AUTHOR: Massimo Lauria <lauria@kth.se>
-COPYRIGHT: Massimo Lauria, 2012
+-------------
+CNF generator
+-------------
 
-The module contains facilities to generate cnf formulas in dimacs
-format. Such formulas are ready to be fed to sat solvers.
+The module  `cnfgen` contains facilities to generate  cnf formulas, in
+order  to be printed  in dimacs  or LaTeX  formats. Such  formulas are
+ready to be  fed to sat solvers.  In  particular the module implements
+both a library of CNF generators and a command line utility.
 
+Copyright (C) 2012  Massimo Lauria <lauria@kth.se>
+https://github.com/MassimoLauria/cnfgen.git
+
+
+Create you own CNFs:
+
+>>> c=CNF([ [(True,"x1"),(True,"x2"),(False,"x3")], \
+          [(False,"x2"),(True,"x4")] ])
+>>> c.dimacs(output_header=False)
+p cnf 4 2
+1 2 -3 0
+-2 4 0
+
+You can add clauses later in the process:
+
+>>> c.add_clause( [(False,"x3"),(True,"x4"),(False,"x5")] )
+>>> c.dimacs(output_header=False)
+p cnf 5 3
+1 2 -3 0
+-2 4 0
+-3 4 -5 0
+
+`cnfgen` module contains a lot of prepackaged CNF generator
+
+>>>
 """
 
 from __future__ import print_function
 import itertools
 
+_default_header="""
+Generated with `cnfgen` (C) Massimo Lauria <lauria@kth.se>
+https://github.com/MassimoLauria/cnfgen.git
+"""
 
 class CNF(object):
-    """Formula in conjunctive normal form
+    """Propositional formulas in conjunctive normal form.
     """
 
-    def __init__(self, clauses=[]):
-        """Build the CNF with an optional set of initial clauses
+    def __init__(self, clauses_and_comments=[],header=None):
+        """Propositional formulas in conjunctive normal form.
 
         To add commented clauses use the `add_clause` and
         `add_comment` methods.
@@ -30,24 +61,32 @@ class CNF(object):
                      variable names.
 
                      E.g. (not x3) or x4 or (not x2) is encoded as (False,u"x3",True,u"x4",False,u"x2")
+        - `header`: a preamble which documents the formula
         """
         self._clauses = []
         self._name_to_id = {}
         self._id_to_name = {}
+        if header:
+            self._header=header
+        else:
+            self._header=_default_header
 
-        for c in clauses:
-            if type(c)==str:
-                self.add_comment(c)
-            else:
-                self.add_clause(c)
+        for c in clauses_and_comments:
+            self.add_clause_or_comment(c)
 
+    def _set_header(self, value):
+        self._header = value
+
+    def _get_header(self):
+        return self._header
+
+    header = property(_get_header, _set_header)
 
     def add_clause(self,clause):
         """Add a well formatted clause to the CNF. It raises
            `ValueError` if the clause is not well formatted.
 
         Arguments:
-        - `self`:
         - `clause`: a clause with k literals is a list with k pairs.
                     First coords are the polarities, second coords are
                     utf8 encoded strings with variable names.
@@ -75,7 +114,6 @@ class CNF(object):
         the variable in a nice order than the appearence one.
 
         Arguments:
-        - `self`:
         - `var`: the name of the variable to add (string).
         """
         if type(var) != str:
@@ -87,14 +125,58 @@ class CNF(object):
             self._id_to_name[id]=var
 
     def add_comment(self,comment):
-        """Add a comment to the formula. Useful for documenting DIMACS format cnfs.
+        """Add a comment to the formula.
+
+        This is useful for documenting cnfs in DIMACS format which may
+        be  quite  cryptic.   Notice  that  you  have  the  option  to
+        intersperse comments  among the clauses,  but that may  not be
+        supported by  your tool. Anyway  comments can be  removed from
+        you Dimacs/LaTeX output.
 
         Arguments:
         - `comment`: an unicode string of text.
+
+        >>> c=CNF()
+        >>> c.add_comment("First clause")
+        >>> c.add_clause([(True,"x"),(False,"y")])
+        >>> c.add_comment("Second clause")
+        >>> c.add_clause([(True,"y"),(False,"z")])
+        >>> c.dimacs(output_header=False,output_comments=True)
+        p cnf 3 2
+        c First clause
+        1 -2 0
+        c Second clause
+        2 -3 0
+        >>> c.dimacs(output_header=False,output_comments=False)
+        p cnf 3 2
+        1 -2 0
+        2 -3 0
         """
         self._clauses.append(comment[:])
 
-    def dimacs(self,outputfile=None):
+    def add_clause_or_comment(self, data):
+        """Add a clause or a comment to the formula
+
+        Arguments:
+        - `data`: either a clause or a comment.
+
+        >>> c=CNF()
+        >>> data=["Hej",[(False,"x"),(True,"y")],"Hej da"]
+        >>> for d in data: c.add_clause_or_comment(d)
+        >>> c.dimacs(output_header=False)
+        p cnf 2 1
+        c Hej
+        -1 2 0
+        c Hej da
+        """
+        if type(data)==str:
+            self.add_comment(data)
+        else:
+            self.add_clause(data)
+
+
+
+    def dimacs(self,outputfile=None,output_header=True,output_comments=True):
         """
         Produce the dimacs encoding of the formula
         """
@@ -104,15 +186,16 @@ class CNF(object):
         m = len([c for c in self._clauses if type(c)!=str])
 
         # A nice header
-        print(u"c Formula generated by cnfgen.")
-        print(u"c \"cnfgen\" has been written by Massimo Lauria. Copyright, 2012")
-        print(u"c")
+        if output_header:
+            for s in self.header.split("\n"): print(u"c "+s)
+
+        # Formula specification
         print(u"p cnf {0} {1}".format(n,m))
 
         # We produce clauses and comments
         for c in self._clauses:
             if type(c)==str:
-                print(u"c "+c)               # output the comment
+                if output_comments: print(u"c "+c)
             else:
                 for neg,var in c:
                     v = self._name_to_id[var]
@@ -120,15 +203,110 @@ class CNF(object):
                     print(u"{0} ".format(v),end="")
                 print(u"0")
 
+    def latex(self,outputfile=None,output_header=True,output_comments=True):
+        """
+        Produce the LaTeX version of the formula
 
-def PHP(pigeons,holes,functional=False,onto=False):
-    """Generates the clauses of pigeonhole principle CNF encoding
+        >>> c=CNF([[(True,"x_1"),(False,"x_2"),(True,"x_3")], \
+                   [(True,"x_2"),(True,"x_4")], \
+                   [(False,"x_2"),(False,"x_3"),(True,"x_4")]])
+        >>> c.latex()
+        %
+        % Generated with `cnfgen` (C) Massimo Lauria <lauria@kth.se>
+        % https://github.com/MassimoLauria/cnfgen.git
+        %
+        \ensuremath{%
+              \\left( \\neg{x_1} \\lor     {x_2} \\lor \\neg{x_3} \\right)
+        \\land \\left( \\neg{x_2} \\lor \\neg{x_4} \\right)
+        \\land \\left(     {x_2} \\lor     {x_3} \\lor \\neg{x_4} \\right)}
+        >>> c=CNF()
+        >>> c.latex(output_header=False)
+        \ensuremath{\square}
+        """
+
+        # A nice header
+        if output_header:
+            for s in self.header.split("\n"): print((u"% "+s).strip())
+
+        # We produce clauses and comments
+        if len(self._clauses)==0:
+            print("\\ensuremath{\\square}")
+            return
+
+        # map literals (neg,var) to latex formulas
+        def map_literals(l):
+            if l[0]: return "\\neg{%s}"%l[1]
+            else: return "    {%s}"%l[1]
+
+        print("\ensuremath{%",end="")
+        first_clause=True
+
+        for c in self._clauses:
+            if type(c)==str:
+                if output_comments: print((u"% "+c).strip())
+            else:
+                if first_clause: print("\n      ",end="")
+                else: print("\n\\land ",end="")
+                # build the latex clause
+                print("\\left( " + \
+                      " \\lor ".join(map(map_literals,c)) + \
+                      " \\right)",end="")
+                first_clause=False
+        print("}")
+
+
+def PigeonholePrinciple(pigeons,holes,functional=False,onto=False):
+    """Pigeonhole Principle CNF formula
+
+    The pigeonhole  principle claims  that no M  pigeons can sit  in N
+    pigeonholes  without collision  if M>N.   The  counterpositive CNF
+    formulation  requires  such mapping  to  be  satisfied. There  are
+    different  variants of this  formula, depending  on the  values of
+    `functional` and `onto` argument.
+
+    - PHP: pigeon can sit in multiple holes
+    - FPHP: each pigeon sits in exactly one hole
+    - onto-PHP: pigeon can  sit in multiple holes, every  hole must be
+                covered.
+    - Matching: one-to-one bijection between pigeons and holes.
 
     Arguments:
     - `pigeon`: number of pigeons
     - `hole`:   number of holes
     - `functional`: add clauses to enforce at most one hole per pigeon
     - `onto`: add clauses to enforce that any hole must have a pigeon
+
+    >>> PigeonholePrinciple(4,3).dimacs(output_header=False,output_comments=True)
+    p cnf 12 22
+    c Pigeon axiom: pigeon 1 sits in a hole
+    1 2 3 0
+    c Pigeon axiom: pigeon 2 sits in a hole
+    4 5 6 0
+    c Pigeon axiom: pigeon 3 sits in a hole
+    7 8 9 0
+    c Pigeon axiom: pigeon 4 sits in a hole
+    10 11 12 0
+    c No collision in hole 1
+    -1 -4 0
+    -1 -7 0
+    -1 -10 0
+    -4 -7 0
+    -4 -10 0
+    -7 -10 0
+    c No collision in hole 2
+    -2 -5 0
+    -2 -8 0
+    -2 -11 0
+    -5 -8 0
+    -5 -11 0
+    -8 -11 0
+    c No collision in hole 3
+    -3 -6 0
+    -3 -9 0
+    -3 -12 0
+    -6 -9 0
+    -6 -12 0
+    -9 -12 0
     """
     if functional:
         if onto:
@@ -141,51 +319,58 @@ def PHP(pigeons,holes,functional=False,onto=False):
         else:
             formula_name="Pigeonhole principle"
 
-    yield "{0} formula for {1} pigeons and {2} holes".format(formula_name,pigeons,holes)
-
-    # Pigeon axioms
-    for p in xrange(1,pigeons+1):
-        yield "Pigeon axiom: pigeon {0} sits in a hole".format(p)
-        yield [ (True,'p_{{{0},{1}}}'.format(p,h)) for h in xrange(1,holes+1)]
-
-    # Onto axioms
-    if onto:
-        for h in xrange(1,holes+1):
-            yield "Onto hole axiom: hole {0} hosts a pigeon".format(h)
-            yield [ (True,'p_{{{0},{1}}}'.format(p,h)) for p in xrange(1,pigeons+1)]
-
-    # No conflicts axioms
-    for h in xrange(1,holes+1):
-        yield "No collision in hole {0}".format(h)
-        for (p1,p2) in itertools.combinations(range(1,pigeons+1),2):
-            yield [ (False,'p_{{{0},{1}}}'.format(p1,h)),
-                    (False,'p_{{{0},{1}}}'.format(p2,h)) ]
-
-    # Function axioms
-    if functional:
+    # Clause generator
+    def _PHP_clause_generator(pigeons,holes,functional,onto):
+        # Pigeon axioms
         for p in xrange(1,pigeons+1):
-            yield "No multiple images for pigeon {0}".format(p)
-            for (h1,h2) in itertools.combinations(range(1,holes+1),2):
-                yield [ (False,'p_{{{0},{1}}}'.format(p,h1)),
-                        (False,'p_{{{0},{1}}}'.format(p,h2)) ]
+            yield "Pigeon axiom: pigeon {0} sits in a hole".format(p)
+            yield [ (True,'p_{{{0},{1}}}'.format(p,h)) for h in xrange(1,holes+1)]
+        # Onto axioms
+        if onto:
+            for h in xrange(1,holes+1):
+                yield "Onto hole axiom: hole {0} hosts a pigeon".format(h)
+                yield [ (True,'p_{{{0},{1}}}'.format(p,h)) for p in xrange(1,pigeons+1)]
+        # No conflicts axioms
+        for h in xrange(1,holes+1):
+            yield "No collision in hole {0}".format(h)
+            for (p1,p2) in itertools.combinations(range(1,pigeons+1),2):
+                yield [ (False,'p_{{{0},{1}}}'.format(p1,h)),
+                        (False,'p_{{{0},{1}}}'.format(p2,h)) ]
+        # Function axioms
+        if functional:
+            for p in xrange(1,pigeons+1):
+                yield "No multiple images for pigeon {0}".format(p)
+                for (h1,h2) in itertools.combinations(range(1,holes+1),2):
+                    yield [ (False,'p_{{{0},{1}}}'.format(p,h1)),
+                            (False,'p_{{{0},{1}}}'.format(p,h2)) ]
 
-def GT(size,total=False):
+    php=CNF();
+    php.header="{0} formula for {1} pigeons and {2} holes\n".format(formula_name,pigeons,holes) + php.header
+
+    clauses=_PHP_clause_generator(pigeons,holes,functional,onto)
+    for c in clauses:
+        php.add_clause_or_comment(c)
+
+    return php
+
+
+
+def OrderingPrinciple(size,total=False):
     """Generates the clauses of ordering principle
 
     Arguments:
     - `size`:   numer of elements
     - `total`:  add clauses to enforce totality
     """
+    gt=CNF()
     # Describe the formula
     if total:
-        yield "Total ordering principle for the graph:"
+        gt.header="Total ordering principle on domain of size %s\n"%size+gt.header
     else:
-        yield "Ordering principle for the graph:"
-    yield ""
-    yield ""
+        gt.header="Ordering principle on domain of size %s\n"%size+gt.header
 
     # Non minimality axioms
-    yield "Each vertex has a predecessor"
+    gt.add_comment("Each vertex has a predecessor")
     for v in xrange(size):
         clause = []
         for u in xrange(v):
@@ -195,35 +380,37 @@ def GT(size,total=False):
                 clause += [(False,'x_{{{0},{1}}}'.format(v,w))]
             else:
                 clause += [(True,'x_{{{0},{1}}}'.format(w,v))]
-        yield clause
+        gt.add_clause(clause)
 
     # Transitivity axiom
-    yield "Relation must be transitive"
+    gt.add_comment("Relation must be transitive")
 
     if size>=3:
         if total:
             # Optimized version if totality is included (less formulas)
             for (v1,v2,v3) in itertools.combinations(xrange(size),3):
-                yield [ (True,'x_{{{0},{1}}}'.format(v1,v2)),
-                        (True,'x_{{{0},{1}}}'.format(v2,v3)),
-                        (False,'x_{{{0},{1}}}'.format(v1,v3))]
-                yield [ (False,'x_{{{0},{1}}}'.format(v1,v2)),
-                        (False,'x_{{{0},{1}}}'.format(v2,v3)),
-                        (True,'x_{{{0},{1}}}'.format(v1,v3))]
+                gt.add_clause([ (True,'x_{{{0},{1}}}'.format(v1,v2)),
+                                (True,'x_{{{0},{1}}}'.format(v2,v3)),
+                                (False,'x_{{{0},{1}}}'.format(v1,v3))])
+                gt.add_clause([ (False,'x_{{{0},{1}}}'.format(v1,v2)),
+                                (False,'x_{{{0},{1}}}'.format(v2,v3)),
+                                (True,'x_{{{0},{1}}}'.format(v1,v3))])
         else:
             for (v1,v2,v3) in itertools.permutations(xrange(size),3):
-                yield [ (False,'x_{{{0},{1}}}'.format(v1,v2)),
-                        (False,'x_{{{0},{1}}}'.format(v2,v3)),
-                        (True, 'x_{{{0},{1}}}'.format(v1,v3))]
+                gt.add_clause([ (False,'x_{{{0},{1}}}'.format(v1,v2)),
+                                (False,'x_{{{0},{1}}}'.format(v2,v3)),
+                                (True, 'x_{{{0},{1}}}'.format(v1,v3))])
 
     # Antisymmetry axioms
     if not total:
-        yield "Relation must be anti-symmetric"
+        gt.add_comment("Relation must be anti-symmetric")
         for (v1,v2) in itertools.permutations(xrange(size),2):
-            yield [ (False,'x_{{{0},{1}}}'.format(v1,v2)),
-                    (False,'x_{{{0},{1}}}'.format(v2,v1))]
+            gt.add_clause([ (False,'x_{{{0},{1}}}'.format(v1,v2)),
+                            (False,'x_{{{0},{1}}}'.format(v2,v1))])
 
-def RAM(s,k,N):
+    return gt
+
+def RamseyNumber(s,k,N):
     """Formula claiming that Ramsey number r(s,k) > N
 
     Arguments:
@@ -232,25 +419,32 @@ def RAM(s,k,N):
     - `N`: vertices
     """
 
-    yield "CNF encoding of the claim that:"
-    yield "there is a graph of N vertices with no"
-    yield " clique of size k and not indipendent set of size s."
+    ram=CNF()
+
+    ram.header="""
+CNF encoding of the claim that there is a graph of %d vertices with no
+indipendent set of size %d and no clique of size %d
+""" % (s,k,N) + ram.header
 
     # No independent set of size s
+    ram.add_comment("No independent set of size %d" % s)
+
     for vertex_set in itertools.combinations(xrange(1,N+1),s):
         clause=[]
         for edge in itertools.combinations(vertex_set,2):
             clause+=[(True,'e_{{{0},{1}}}'.format(*edge))]
-        yield clause
+        ram.add_clause(clause)
 
     # No clique of size k
+    ram.add_comment("No clique of size %d"%k)
+
     for vertex_set in itertools.combinations(xrange(1,N+1),k):
         clause=[]
         for edge in itertools.combinations(vertex_set,2):
             clause+=[(False,'e_{{{0},{1}}}'.format(*edge))]
-        yield clause
+        ram.add_clause(clause)
 
-
+    return ram
 
 class _Graph(object):
     """Simple graph class for internal representation.
