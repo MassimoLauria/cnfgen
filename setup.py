c83be93c579be04a9ab585531e9db7528ec3cc4d
Massimo Lauria <lauria.massimo@gmail.com>
2015-02-12 22:30:55 +0100

    Improving setup script.

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
+#!/usr/bin/env python
+"""
+Setup script for the CNFgen package
+"""
+
+
 from setuptools import setup
 
-setup(name='CnfGen',
+setup(name='CNFgen',
       version='0.5',
       description='CNF formula generator',
       author='Massimo Lauria',
       author_email='lauria@kth.se',
       url='https://github.com/MassimoLauria/cnfgen',
       packages =['cnfformula'],
+      license = 'GPL-3',
       entry_points = {
           'console_scripts': ['cnfgen=cnfformula.cnfgen:command_line_utility'],
-      }
+      },
+      install_requires=[
+          'networkx',
+      ]
      )
