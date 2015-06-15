340637c3f452584507c08bcc95257f44968ea1ef
Massimo Lauria <lauria.massimo@gmail.com>
2015-06-14 22:39:55 -0700

    Bug fixed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 
 setup(name='CNFgen',
-      version='0.5.4.2',
+      version='0.5.4.3',
       description='CNF formula generator',
       author='Massimo Lauria',
       author_email='lauria.massimo@gmail.com',
