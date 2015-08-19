6f9acf7fe91d61c8a6e8d5bb53726bdfd3fc7e0e
Marc Vinyals <vinyals@kth.se>
2015-08-19 22:47:57 +0200

    Add test for cnfgen command line tool

---
+++ cnfformula/tests/test_cnfgen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import unittest
+import cnfformula.cnfgen as cnfgen
+
+class TestCnfgen(unittest.TestCase):
+    def test_empty(self):
+        with self.assertRaises(SystemExit) as cm:
+            cnfgen.command_line_utility(["cnfgen"])
+        self.assertNotEqual(cm.exception.code, 0)
+
+    def test_help(self):
+        with self.assertRaises(SystemExit) as cm:
+            cnfgen.command_line_utility(["cnfgen","-h"])
+        self.assertEqual(cm.exception.code, 0)
+
+    def test_find_formula_families(self):
+        subcommands = cnfgen.find_formula_families()
+        self.assertNotEqual(subcommands[:],[])
+        
+    def test_subformulas_help(self):
+        subcommands = cnfgen.find_formula_families()
+        for sc in subcommands:
+            with self.assertRaises(SystemExit) as cm:
+                cnfgen.command_line_utility(["cnfgen", sc.name, "-h"])
+            self.assertEqual(cm.exception.code, 0)
+
+    def test_nonformula_empty(self):
+        with self.assertRaises(SystemExit) as cm:
+            cnfgen.command_line_utility(["cnfgen", "spam"])
+        self.assertNotEqual(cm.exception.code, 0)
+
+    def test_nonformula_help(self):
+        with self.assertRaises(SystemExit) as cm:
+            cnfgen.command_line_utility(["cnfgen", "spam", "-h"])
+        self.assertNotEqual(cm.exception.code, 0)
