da96c28674cb2a7fad1d86f480bb42586f877a40
Marc Vinyals <vinyals@kth.se>
2015-09-01 14:53:51 +0200

    Generate dense random CNFs

--- cnfformula/families/randomformulas.py
+++ cnfformula/families/randomformulas.py
@@ -6,12 +6,32 @@ Copyright (C) 2012, 2013, 2014, 2015  Massimo Lauria <lauria@kth.se>
 https://github.com/MassimoLauria/cnfgen.git
 """
 
+import itertools
+import random
+
 from cnfformula.cnf import CNF
 
 import cnfformula.cmdline
 import cnfformula.families
 
+def sample_clauses(k, indices, m):
+    clauses = set()
+    t = 0
+    while len(clauses)<m and t < 10*m:
+        t += 1
+        clauses.add(tuple((random.choice([True,False]),'x_{0}'.format(i))
+                          for i in sorted(random.sample(indices,k))))
+    if len(clauses)<m:
+        return sample_clauses_dense(k, indices, m)
+    return clauses
 
+def all_clauses(k, indices):
+    for domain in itertools.combinations(indices, k):
+        for polarity in itertools.product([True,False], repeat=3):
+            yield zip(polarity,('x_{0}'.format(i) for i in domain))
+
+def sample_clauses_dense(k, indices, m):
+    return random.sample(list(all_clauses(k, indices)), m)
 
 @cnfformula.families.register_cnf_generator
 def RandomKCNF(k, n, m, seed=None):
@@ -52,7 +72,6 @@ def RandomKCNF(k, n, m, seed=None):
         the formula is too dense for the simple sampling process.
 
     """
-    import random
     if seed:
         random.seed(seed)
 
@@ -69,16 +88,7 @@ def RandomKCNF(k, n, m, seed=None):
     indices = xrange(1,n+1) 
     for i in indices:
         F.add_variable('x_{0}'.format(i))
-
-    clauses = set()
-    t = 0
-    while len(clauses)<m and t < 10*m:
-        t += 1
-        clauses.add(tuple((random.choice([True,False]),'x_{0}'.format(i))
-                          for i in sorted(random.sample(indices,k))))
-    if len(clauses)<m:
-        raise RuntimeError("Sampling is taking too long. Maybe the requested formula is too dense.")
-    for clause in clauses:
+    for clause in sample_clauses(k, indices, m):
         F.add_clause(list(clause),strict=True)
 
     return F
--- cnfformula/tests/test_randomcnf.py
+++ cnfformula/tests/test_randomcnf.py
@@ -1,20 +1,44 @@
 from cnfformula import RandomKCNF
+from cnfformula.families.randomformulas import all_clauses
 
 from . import TestCNFBase
 
 class TestRandomCNF(TestCNFBase) :
+    def check_random_cnf(self,width,numvariables,numclauses) :
+        F = RandomKCNF(width,numvariables,numclauses)
+        self.assertListEqual(list(F.variables()),
+                             ['x_{0}'.format(i) for i in range(1,numvariables+1)])
+        self.assertEqual(len(F),numclauses)
+        self.assertEqual(len(set(frozenset(x) for x in F.clauses())),numclauses)
+
     def test_empty_cnf(self) :
-        F = RandomKCNF(0,0,0)
-        self.assertListEqual(list(F.variables()),[])
-        self.assertListEqual(list(F.clauses()),[])
+        self.check_random_cnf(0,0,0)
 
     def test_empty_cnf_with_vars(self) :
-        F = RandomKCNF(0,10,0)
-        self.assertListEqual(list(F.variables()),['x_{0}'.format(i) for i in range(1,11)])
-        self.assertListEqual(list(F.clauses()),[])
+        self.check_random_cnf(0,10,0)
 
-    def test_random_cnf(self) :
-        F = RandomKCNF(3,10,50)
-        self.assertListEqual(list(F.variables()),['x_{0}'.format(i) for i in range(1,11)])
-        self.assertEqual(len(F),50)
-        self.assertEqual(len(set(frozenset(x) for x in F.clauses())),50)
+    def test_random_cnf_medium(self) :
+        self.check_random_cnf(3,10,50)
+
+    def test_full(self):
+        self.check_random_cnf(3,5,5*4*3/(1*2*3)*2**3)
+
+    def test_too_full(self):
+        with self.assertRaises(ValueError):
+            RandomKCNF(3,5,5*4*3/(1*2*3)*2**5+1)
+
+    def test_too_wide(self):
+        with self.assertRaises(ValueError):
+            RandomKCNF(10,5,1)
+
+    def test_negative_width(self):
+        with self.assertRaises(ValueError):
+            RandomKCNF(-1,5,1)
+
+    def test_negative_variables(self):
+        with self.assertRaises(ValueError):
+            RandomKCNF(3,-1,1)
+
+    def test_negative_clauses(self):
+        with self.assertRaises(ValueError):
+            RandomKCNF(3,5,-1)
