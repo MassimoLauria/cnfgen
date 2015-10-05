7c373be5426f1496377464aa2ec9a649fc5680be
Massimo Lauria <lauria.massimo@gmail.com>
2015-10-05 16:29:04 +0900

    Random CNF with planted assignment.

--- cnfformula/families/randomformulas.py
+++ cnfformula/families/randomformulas.py
@@ -11,27 +11,55 @@ from cnfformula.cnf import CNF
 import cnfformula.cmdline
 import cnfformula.families
 
-def sample_clauses(k, indices, m):
+
+
+def clause_satisfied(cls,assignments):
+    """Test whether a clause is satisfied by all assignments
+
+Test if clauses `cls` is satisfied by all assigment in the
+list assignments.
+"""
+    for assignment in assignments:
+
+        if  any( assignment.has_key(var) and polarity == assignment[var]
+                 for (polarity, var) in cls):
+            continue
+
+        else:
+            return False
+
+    return True
+
+def sample_clauses(k, indices, m, planted_assignments):
     clauses = set()
     t = 0
     while len(clauses)<m and t < 10*m:
         t += 1
-        clauses.add(tuple((random.choice([True,False]),'x_{0}'.format(i))
-                          for i in sorted(random.sample(indices,k))))
+
+        cls = tuple((random.choice([True,False]),'x_{0}'.format(i))
+                    for i in sorted(random.sample(indices,k)))
+
+        if clause_satisfied(cls,planted_assignments):
+            clauses.add(cls)
+        
     if len(clauses)<m:
-        return sample_clauses_dense(k, indices, m)
+        return sample_clauses_dense(k, indices, m, planted_assignments)
     return clauses
 
-def all_clauses(k, indices):
+def all_clauses(k, indices, planted_assignments):
     for domain in itertools.combinations(indices, k):
         for polarity in itertools.product([True,False], repeat=k):
-            yield zip(polarity,('x_{0}'.format(i) for i in domain))
 
-def sample_clauses_dense(k, indices, m):
-    return random.sample(list(all_clauses(k, indices)), m)
+            cls =  zip(polarity,('x_{0}'.format(i) for i in domain))
+            if clause_satisfied(cls, planted_assignments):
+                yield cls
+
+                
+def sample_clauses_dense(k, indices, m, planted_assignments):
+    return random.sample(list(all_clauses(k, indices, planted_assignments)), m)
 
 @cnfformula.families.register_cnf_generator
-def RandomKCNF(k, n, m, seed=None):
+def RandomKCNF(k, n, m, seed=None, planted_assignments=[]):
     """Build a random k-CNF
 
     Sample :math:`m` clauses over :math:`n` variables, each of width
@@ -43,16 +71,20 @@ def RandomKCNF(k, n, m, seed=None):
     ----------
     k : int
        width of each clause
-    
+
     n : int
-       number of variables to choose from. The resulting CNF object 
+       number of variables to choose from. The resulting CNF object
        will contain n variables even if some are not mentioned in the clauses.
-    
+
     m : int
        number of clauses to generate
-    
+
     seed : hashable object
        seed of the random generator
+
+    planted_assignments : iterable(dict), optional 
+       a set of total/partial assigments such that all clauses in the formula 
+       will be satisfied by all of them.
 
     Returns
     -------
@@ -60,7 +92,7 @@ def RandomKCNF(k, n, m, seed=None):
 
     Raises
     ------
-    ValueError 
+    ValueError
         when some paramenter is negative, or when k>n.
 
     """
@@ -73,18 +105,21 @@ def RandomKCNF(k, n, m, seed=None):
 
     if k>n:
         raise ValueError("Clauses cannot have more {} literals.".format(n))
-        
+
     F = CNF()
     F.header = "Random {}-CNF over {} variables and {} clauses\n".format(k,n,m) + F.header
 
-    indices = xrange(1,n+1) 
+    indices = xrange(1,n+1)
     for i in indices:
         F.add_variable('x_{0}'.format(i))
-    for clause in sample_clauses(k, indices, m):
-        F.add_clause(list(clause),strict=True)
+    try:
+        for clause in sample_clauses(k, indices, m, planted_assignments):
+            F.add_clause(list(clause), strict=True)
+    except ValueError:
+        raise ValueError("There are fewer clauses available than the number requested")
 
     return F
- 
+
 
 @cnfformula.cmdline.register_cnfgen_subcommand
 class RandCmdHelper(object):
@@ -111,5 +146,4 @@ class RandCmdHelper(object):
         Arguments:
         - `args`: command line options
         """
-        return RandomKCNF(args.k,args.n,args.m)
-
+        return RandomKCNF(args.k, args.n, args.m)
