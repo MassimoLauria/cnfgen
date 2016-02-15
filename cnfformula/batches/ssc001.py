0fbe1ef945f06896b9c9306b033ba7a6be620964
Massimo Lauria <lauria.massimo@gmail.com>
2016-02-15 21:32:46 +0100

    ssc001 now is UNSAT and larger

--- cnfformula/batches/ssc001.py
+++ cnfformula/batches/ssc001.py
@@ -5,7 +5,7 @@
 
 from cnfformula.cnfgen import command_line_utility as cmd
 from cnfformula.batches import register_cnf_batch
-from cnfformula.graphs import writeGraph,bipartite_random_regular
+from cnfformula.graphs import writeGraph,bipartite_random_regular,sample_missing_edges
 
 import random
 
@@ -13,7 +13,7 @@ import random
 class SSC001(object):
 
     name='ssc001'
-    description='Subset cardinality formulas (4 entries per row/col +1 entry) on NxN N=5,10,..,50'
+    description='Subset cardinality formulas (4 entries per row/col +1 entry) on NxN N=50,100,..,1000'
 
     random_seed = 734841396579L
     
@@ -26,7 +26,7 @@ Each formula is on an NxN matrix with 4 non empty entries per row and per column
 Matrix are saved along with the DIMACS files.
 """)
 
-        for s in xrange(5,51,5):
+        for s in xrange(50,1001,50):
 
             fname = "ssc001size{}.cnf".format(s)
             gname = "ssc001size{}.matrix".format(s)
@@ -34,6 +34,6 @@ Matrix are saved along with the DIMACS files.
             print("-- {} and {}".format(gname,fname))
 
             G = bipartite_random_regular(s,s,4)
-            
+            G.add_edges_from(sample_missing_edges(G,1))
             writeGraph(G,gname,graph_type="bipartite")
             cmd(["cnfgen","-o",fname,"subsetcard","-i",gname])
