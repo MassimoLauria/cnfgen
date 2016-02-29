69df58b49d540f1a16ff7b392bfcc7d8a6f6c773
Marc Vinyals <vinyals@kth.se>
2016-02-29 19:12:34 +0100

    Add tests for graph modifications

--- cnfformula/tests/test_graph_cmdline.py
+++ cnfformula/tests/test_graph_cmdline.py
@@ -65,6 +65,52 @@ class TestBipartite(TestArgparse) :
         for v in right:
             self.assertEqual(G.degree(v),10)
 
+class TestAddBiEdges(TestArgparse) :
+    def setUp(self):
+        self.graph_helper = cnfformula.cmdline.BipartiteGraphHelper()
+
+    def test_already_complete(self):
+        with self.assertRaises(ValueError):
+            self.parse(["--bcomplete", "10", "9", "--addedges", "1"])
+
+    def test_complete(self):
+        G = self.parse(["--bp", "10", "9", "0", "--addedges", "90"])
+        self.assertEqual(G.order(),19)
+        self.assertEqual(G.size(),90)
+
+    def test_add(self):
+        G = self.parse(["--bm", "10", "9", "15", "--addedges", "10"])
+        self.assertEqual(G.order(),19)
+        self.assertEqual(G.size(),25)
+
+class TestPlantBiClique(TestArgparse) :
+    def setUp(self):
+        self.graph_helper = cnfformula.cmdline.BipartiteGraphHelper()
+
+    def test_too_large_left(self):
+        with self.assertRaises(ValueError):
+            self.parse(["--bcomplete", "10", "9", "--plantbiclique", "11", "9"])
+
+    def test_too_large_right(self):
+        with self.assertRaises(ValueError):
+            self.parse(["--bcomplete", "10", "9", "--plantbiclique", "10", "10"])
+
+    def test_already_complete(self):
+        G = self.parse(["--bcomplete", "10", "9", "--plantbiclique", "5", "4"])
+        self.assertEqual(G.order(),19)
+        self.assertEqual(G.size(),90)
+
+    def test_complete(self):
+        G = self.parse(["--bp", "10", "9", "0", "--plantbiclique", "10", "9"])
+        self.assertEqual(G.order(),19)
+        self.assertEqual(G.size(),90)
+
+    def test_add(self):
+        G = self.parse(["--bm", "10", "9", "15", "--plantbiclique", "5", "4"])
+        self.assertEqual(G.order(),19)
+        self.assertGreaterEqual(G.size(),20)
+        self.assertLessEqual(G.size(),35)
+
 class TestSimple(TestArgparse) :
     def setUp(self):
         self.graph_helper = cnfformula.cmdline.SimpleGraphHelper()
@@ -99,3 +145,45 @@ class TestSimple(TestArgparse) :
         G = self.parse(["--torus", "8", "5"])
         self.assertEqual(G.order(),40)
         self.assertListEqual(G.degree().values(),[4]*40)
+
+class TestAddEdges(TestArgparse) :
+    def setUp(self):
+        self.graph_helper = cnfformula.cmdline.SimpleGraphHelper()
+
+    def test_already_complete(self):
+        with self.assertRaises(ValueError):
+            self.parse(["--complete", "10", "--addedges", "1"])
+
+    def test_complete(self):
+        G = self.parse(["--gnp", "10", "0", "--addedges", "45"])
+        self.assertEqual(G.order(),10)
+        self.assertEqual(G.size(),45)
+
+    def test_add(self):
+        G = self.parse(["--gnm", "10", "15", "--addedges", "10"])
+        self.assertEqual(G.order(),10)
+        self.assertEqual(G.size(),25)
+
+class TestPlantClique(TestArgparse) :
+    def setUp(self):
+        self.graph_helper = cnfformula.cmdline.SimpleGraphHelper()
+
+    def test_too_large(self):
+        with self.assertRaises(ValueError):
+            self.parse(["--complete", "10", "--plantclique", "11"])
+
+    def test_already_complete(self):
+        G = self.parse(["--complete", "10", "--plantclique", "5"])
+        self.assertEqual(G.order(),10)
+        self.assertEqual(G.size(),45)
+
+    def test_complete(self):
+        G = self.parse(["--gnp", "10", "0", "--plantclique", "10"])
+        self.assertEqual(G.order(),10)
+        self.assertEqual(G.size(),45)
+
+    def test_add(self):
+        G = self.parse(["--gnm", "10", "15", "--plantclique", "5"])
+        self.assertEqual(G.order(),10)
+        self.assertGreaterEqual(G.size(),20)
+        self.assertLessEqual(G.size(),35)
