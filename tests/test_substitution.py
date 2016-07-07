605edfdfccc15e2bfd9d9102a6c450d9453d6048
Massimo Lauria <lauria.massimo@gmail.com>
2016-07-07 02:01:16 +0200

    Import stuff properly.

--- tests/test_substitution.py
+++ tests/test_substitution.py
@@ -5,30 +5,30 @@ from . import TestCNFBase
 class TestSubstitution(TestCNFBase) :
     def test_or(self) :
         cnf = cnfformula.CNF([[(True,'x'),(False,'y')]])
-        lift = cnfformula.transformations.substitutions.OrSubstitution(cnf, 3)
+        lift = cnfformula.OrSubstitution(cnf, 3)
         self.assertCnfEqual(lift,cnfformula.CNF([
                     [(True,'{x}^0'),(True,'{x}^1'),(True,'{x}^2'),(False,'{y}^0')],
                     [(True,'{x}^0'),(True,'{x}^1'),(True,'{x}^2'),(False,'{y}^1')],
                     [(True,'{x}^0'),(True,'{x}^1'),(True,'{x}^2'),(False,'{y}^2')],
                     ]))
-        lift2 = cnfformula.transformations.substitutions.OrSubstitution(cnf, 3)
+        lift2 = cnfformula.OrSubstitution(cnf, 3)
         self.assertCnfEqual(lift,lift2)
 
     def test_xor(self) :
         cnf = cnfformula.CNF([[(True,'x'),(False,'y')]])
-        lift = cnfformula.transformations.substitutions.XorSubstitution(cnf, 2)
+        lift = cnfformula.XorSubstitution(cnf, 2)
         self.assertCnfEqual(lift,cnfformula.CNF([
                     [(True,'{x}^0'),(True,'{x}^1'),(True,'{y}^0'),(False,'{y}^1')],
                     [(False,'{x}^0'),(False,'{x}^1'),(True,'{y}^0'),(False,'{y}^1')],
                     [(True,'{x}^0'),(True,'{x}^1'),(False,'{y}^0'),(True,'{y}^1')],
                     [(False,'{x}^0'),(False,'{x}^1'),(False,'{y}^0'),(True,'{y}^1')],
                     ]))
-        lift2 = cnfformula.transformations.substitutions.XorSubstitution(cnf, 2)
+        lift2 = cnfformula.XorSubstitution(cnf, 2)
         self.assertCnfEqual(lift,lift2)
 
     def test_majority(self) :
         cnf = cnfformula.CNF([[(True,'x'),(False,'y')]])
-        lift = cnfformula.transformations.substitutions.MajoritySubstitution(cnf, 3)
+        lift = cnfformula.MajoritySubstitution(cnf, 3)
         self.assertCnfEqual(lift,cnfformula.CNF([
                     [(True,'{x}^0'),(True,'{x}^1'),(False,'{y}^0'),(False,'{y}^1')],
                     [(True,'{x}^1'),(True,'{x}^2'),(False,'{y}^0'),(False,'{y}^1')],
@@ -40,24 +40,24 @@ class TestSubstitution(TestCNFBase) :
                     [(True,'{x}^1'),(True,'{x}^2'),(False,'{y}^2'),(False,'{y}^0')],
                     [(True,'{x}^2'),(True,'{x}^0'),(False,'{y}^2'),(False,'{y}^0')],
                     ]))
-        lift2 = cnfformula.transformations.substitutions.MajoritySubstitution(cnf, 3)
+        lift2 = cnfformula.MajoritySubstitution(cnf, 3)
         self.assertCnfEqual(lift,lift2)
         
     def test_equality(self) :
         cnf = cnfformula.CNF([[(False,'x'),(True,'y')]])
-        lift = cnfformula.transformations.substitutions.AllEqualSubstitution(cnf, 2)
+        lift = cnfformula.AllEqualSubstitution(cnf, 2)
         self.assertCnfEqual(lift,cnfformula.CNF([
                     [(True,'{x}^0'),(True,'{x}^1'),(True,'{y}^0'),(False,'{y}^1')],
                     [(False,'{x}^0'),(False,'{x}^1'),(True,'{y}^0'),(False,'{y}^1')],
                     [(True,'{x}^0'),(True,'{x}^1'),(False,'{y}^0'),(True,'{y}^1')],
                     [(False,'{x}^0'),(False,'{x}^1'),(False,'{y}^0'),(True,'{y}^1')],
                     ]))
-        lift2 = cnfformula.transformations.substitutions.AllEqualSubstitution(cnf, 2)
+        lift2 = cnfformula.AllEqualSubstitution(cnf, 2)
         self.assertCnfEqual(lift,lift2)
 
     def test_inequality(self) :
         cnf = cnfformula.CNF([[(False,'x'),(True,'y')]])
-        lift = cnfformula.transformations.substitutions.NotAllEqualSubstitution(cnf, 2)
+        lift = cnfformula.NotAllEqualSubstitution(cnf, 2)
         self.assertCnfEqual(lift,cnfformula.CNF([
                     [(True,'{x}^0'),(False,'{x}^1'),(True,'{y}^0'),(True,'{y}^1')],
                     [(False,'{x}^0'),(True,'{x}^1'),(True,'{y}^0'),(True,'{y}^1')],
@@ -67,7 +67,7 @@ class TestSubstitution(TestCNFBase) :
 
     def test_inequality_pos_clause(self) :
         cnf = cnfformula.CNF([[(True,'x')]])
-        lift = cnfformula.transformations.substitutions.NotAllEqualSubstitution(cnf, 3)
+        lift = cnfformula.NotAllEqualSubstitution(cnf, 3)
         self.assertCnfEqual(lift,cnfformula.CNF([
                     [(True,'{x}^0'),(True,'{x}^1'),(True,'{x}^2')],
                     [(False,'{x}^0'),(False,'{x}^1'),(False,'{x}^2')]
@@ -84,24 +84,24 @@ class TestSubstitution(TestCNFBase) :
                 [(False,'z'),(False,'t')],
                 [(True,'u'),(True,'v')]
                 ])
-        lifteq  = cnfformula.transformations.substitutions.AllEqualSubstitution(cnf, 4)
-        liftneq = cnfformula.transformations.substitutions.NotAllEqualSubstitution(cnfneg, 4)
+        lifteq  = cnfformula.AllEqualSubstitution(cnf, 4)
+        liftneq = cnfformula.NotAllEqualSubstitution(cnfneg, 4)
         self.assertCnfEqual(lifteq, liftneq)
 
     def test_if_then_else(self) :
         cnf = cnfformula.CNF([[(True,'x'),(False,'y')]])
-        lift  = cnfformula.transformations.substitutions.IfThenElseSubstitution(cnf)
+        lift  = cnfformula.IfThenElseSubstitution(cnf)
         self.assertCnfEqual(lift,cnfformula.CNF([
                     [(False,'{x}^0'),(True,'{x}^1'),(False,'{y}^0'),(False,'{y}^1')],
                     [(False,'{x}^0'),(True,'{x}^1'),(True,'{y}^0'),(False,'{y}^2')],
                     [(True,'{x}^0'),(True,'{x}^2'),(False,'{y}^0'),(False,'{y}^1')],
                     [(True,'{x}^0'),(True,'{x}^2'),(True,'{y}^0'),(False,'{y}^2')],
         ]))
-        lift2 = cnfformula.transformations.substitutions.IfThenElseSubstitution(cnf)
+        lift2 = cnfformula.IfThenElseSubstitution(cnf)
         self.assertCnfEqual(lift,lift2)
 
     def test_exactly_one(self) :
         cnf = cnfformula.CNF([[(True,'x'),(False,'y')]])
-        lift  = cnfformula.transformations.substitutions.ExactlyOneSubstitution(cnf,2)
-        lift2 = cnfformula.transformations.substitutions.ExactlyOneSubstitution(cnf,2)
+        lift  = cnfformula.ExactlyOneSubstitution(cnf,2)
+        lift2 = cnfformula.ExactlyOneSubstitution(cnf,2)
         self.assertCnfEqual(lift,lift2)
