3308422d0af720c02b1f8c3c997d91e14d5296db
Massimo Lauria <lauria.massimo@gmail.com>
2019-11-26 18:47:12 +0100

    New location of the dimacs2cnf utils

--- tests/test_dimacsparser.py
+++ tests/test_dimacsparser.py
@@ -1,5 +1,5 @@
 import cnfformula
-import cnfformula.utils as cnfutils
+from cnfformula.utils import dimacs2cnf
 
 from .test_cnfformula import TestCNF
 
@@ -9,46 +9,46 @@ class TestDimacsParser(TestCNF) :
     def test_empty_file(self) :
         dimacs = io.StringIO()
         with self.assertRaises(ValueError) :
-            cnfutils.dimacs2cnf(dimacs)
+            dimacs2cnf(dimacs)
 
     def test_empty_cnf(self) :
         dimacs = io.StringIO("p cnf 0 0\n")
-        cnf = cnfutils.dimacs2cnf(dimacs)
+        cnf = dimacs2cnf(dimacs)
         self.assertCnfEqual(cnf,cnfformula.CNF())
 
     def test_comment_only_file(self) :
         dimacs = io.StringIO("c Hej!\n")
         with self.assertRaises(ValueError) :
-            cnfutils.dimacs2cnf(dimacs)
+            dimacs2cnf(dimacs)
 
     def test_invalid_file(self) :
         dimacs = io.StringIO("Hej!\n")
         with self.assertRaises(ValueError) :
-            cnfutils.dimacs2cnf(dimacs)
+            dimacs2cnf(dimacs)
 
     def test_commented_empty_cnf(self) :
         dimacs = io.StringIO("c Hej!\np cnf 0 0\n")
-        cnf = cnfutils.dimacs2cnf(dimacs)
+        cnf = dimacs2cnf(dimacs)
         self.assertCnfEqual(cnf,cnfformula.CNF())
 
     def test_one_clause_cnf(self) :
         dimacs = io.StringIO("c Hej!\np cnf 2 1\n1 -2 0\n")
-        cnf = cnfutils.dimacs2cnf(dimacs)
+        cnf = dimacs2cnf(dimacs)
         self.assertCnfEqual(cnf,cnfformula.CNF([[(True, 1),(False, 2)]]))
 
     def test_one_var_cnf(self) :
         dimacs = io.StringIO("c Hej!\np cnf 1 2\n1 0\n-1 0\n")
-        cnf = cnfutils.dimacs2cnf(dimacs)
+        cnf = dimacs2cnf(dimacs)
         self.assertCnfEqual(cnf,cnfformula.CNF([[(True, 1)],[(False, 1)]]))
     
     def test_inverse(self) :
         cnf = TestCNF.sorted_cnf([[(True,2),(False,1)]])
         dimacs = io.StringIO(cnf.dimacs())
-        cnf2 = cnfutils.dimacs2cnf(dimacs)
+        cnf2 = dimacs2cnf(dimacs)
         self.assertCnfEqual(cnf2,cnf)
 
     def test_inverse_random(self) :
         cnf = self.random_cnf(4,10,100)
         dimacs = io.StringIO(cnf.dimacs())
-        cnf2 = cnfutils.dimacs2cnf(dimacs)
+        cnf2 = dimacs2cnf(dimacs)
         self.assertCnfEqual(cnf2,cnf)
