e7ec54c62da8e3c6ccad1e9b98879d444f62d126
Massimo Lauria <lauria.massimo@gmail.com>
2020-05-15 18:08:02 +0200

    Few edits in conf files

--- conftest.py
+++ conftest.py
@@ -5,7 +5,8 @@ import os
 collect_ignore = ["setup.py"]
 collect_ignore.append("docs/conf.py")
 
+
 @pytest.fixture(autouse=True)
 def tempdir_doctest_file(doctest_namespace, tmpdir):
-    doctest_namespace['tmpdir'] = os.path.join(str(tmpdir),"")
-    print(os.path.join(str(tmpdir),""))
+    doctest_namespace['tmpdir'] = os.path.join(str(tmpdir), "")
+    print(os.path.join(str(tmpdir), ""))
