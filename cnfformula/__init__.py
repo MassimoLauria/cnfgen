ab5cab0c4aafb8832e723523a87add4023bb4396
Massimo Lauria <lauria.massimo@gmail.com>
2020-08-06 01:26:46 +0200

    Make transformation a package

--- cnfformula/__init__.py
+++ cnfformula/__init__.py
@@ -8,55 +8,55 @@ transformations implemented, plus some IO functions.
 """
 
 # Basic CNF object
-from .cnf import CNF
+from cnfformula.cnf import CNF
 
 # IO functions
-from .utils.parsedimacs import readCNF
-from .graphs import readGraph, writeGraph
-from .graphs import supported_formats as supported_graph_formats
+from cnfformula.utils.parsedimacs import readCNF
+from cnfformula.graphs import readGraph, writeGraph
+from cnfformula.graphs import supported_formats as supported_graph_formats
 
 # SAT solvers
-from .utils.solver import supported_satsolvers
-from .utils.solver import some_solver_installed
+from cnfformula.utils.solver import supported_satsolvers
+from cnfformula.utils.solver import some_solver_installed
 
 # Formula families implemented
-from .families.cliquecoloring import CliqueColoring
-from .families.coloring import GraphColoringFormula
-from .families.coloring import EvenColoringFormula
-from .families.counting import CountingPrinciple
-from .families.counting import PerfectMatchingPrinciple
-from .families.dominatingset import DominatingSet
-from .families.graphisomorphism import GraphIsomorphism
-from .families.graphisomorphism import GraphAutomorphism
-from .families.ordering import OrderingPrinciple
-from .families.ordering import GraphOrderingPrinciple
-from .families.pebbling import PebblingFormula
-from .families.pebbling import StoneFormula
-from .families.pebbling import SparseStoneFormula
-from .families.pigeonhole import PigeonholePrinciple
-from .families.pigeonhole import GraphPigeonholePrinciple
-from .families.pigeonhole import BinaryPigeonholePrinciple
-from .families.ramsey import PythagoreanTriples
-from .families.ramsey import RamseyLowerBoundFormula
-from .families.randomformulas import RandomKCNF
-from .families.subgraph import SubgraphFormula
-from .families.subgraph import CliqueFormula
-from .families.subgraph import BinaryCliqueFormula
-from .families.subgraph import RamseyWitnessFormula
-from .families.subsetcardinality import SubsetCardinalityFormula
-from .families.tseitin import TseitinFormula
-from .families.pitfall import PitfallFormula
-from .families.cpls import CPLSFormula
+from cnfformula.families.cliquecoloring import CliqueColoring
+from cnfformula.families.coloring import GraphColoringFormula
+from cnfformula.families.coloring import EvenColoringFormula
+from cnfformula.families.counting import CountingPrinciple
+from cnfformula.families.counting import PerfectMatchingPrinciple
+from cnfformula.families.dominatingset import DominatingSet
+from cnfformula.families.graphisomorphism import GraphIsomorphism
+from cnfformula.families.graphisomorphism import GraphAutomorphism
+from cnfformula.families.ordering import OrderingPrinciple
+from cnfformula.families.ordering import GraphOrderingPrinciple
+from cnfformula.families.pebbling import PebblingFormula
+from cnfformula.families.pebbling import StoneFormula
+from cnfformula.families.pebbling import SparseStoneFormula
+from cnfformula.families.pigeonhole import PigeonholePrinciple
+from cnfformula.families.pigeonhole import GraphPigeonholePrinciple
+from cnfformula.families.pigeonhole import BinaryPigeonholePrinciple
+from cnfformula.families.ramsey import PythagoreanTriples
+from cnfformula.families.ramsey import RamseyLowerBoundFormula
+from cnfformula.families.randomformulas import RandomKCNF
+from cnfformula.families.subgraph import SubgraphFormula
+from cnfformula.families.subgraph import CliqueFormula
+from cnfformula.families.subgraph import BinaryCliqueFormula
+from cnfformula.families.subgraph import RamseyWitnessFormula
+from cnfformula.families.subsetcardinality import SubsetCardinalityFormula
+from cnfformula.families.tseitin import TseitinFormula
+from cnfformula.families.pitfall import PitfallFormula
+from cnfformula.families.cpls import CPLSFormula
 
 # Formula transformation implemented
-from .transformations.substitutions import AllEqualSubstitution
-from .transformations.substitutions import ExactlyOneSubstitution
-from .transformations.substitutions import FlipPolarity
-from .transformations.substitutions import FormulaLifting
-from .transformations.substitutions import IfThenElseSubstitution
-from .transformations.substitutions import MajoritySubstitution
-from .transformations.substitutions import NotAllEqualSubstitution
-from .transformations.substitutions import OrSubstitution
-from .transformations.substitutions import VariableCompression
-from .transformations.substitutions import XorSubstitution
-from .transformations.shuffle import Shuffle
+from cnfformula.transformations.substitutions import AllEqualSubstitution
+from cnfformula.transformations.substitutions import ExactlyOneSubstitution
+from cnfformula.transformations.substitutions import FlipPolarity
+from cnfformula.transformations.substitutions import FormulaLifting
+from cnfformula.transformations.substitutions import IfThenElseSubstitution
+from cnfformula.transformations.substitutions import MajoritySubstitution
+from cnfformula.transformations.substitutions import NotAllEqualSubstitution
+from cnfformula.transformations.substitutions import OrSubstitution
+from cnfformula.transformations.substitutions import VariableCompression
+from cnfformula.transformations.substitutions import XorSubstitution
+from cnfformula.transformations.shuffle import Shuffle
