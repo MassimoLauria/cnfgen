1694c1f1120147b768ad3e9719592dbec0ee30a1
Massimo Lauria <massimo.lauria@uniroma1.it>
2021-07-08 20:19:45 +0200

    Help for pitfall formula

--- cnfgen/clihelpers/pitfall_helpers.py
+++ cnfgen/clihelpers/pitfall_helpers.py
@@ -11,67 +11,48 @@ from cnfgen.families.pitfall import PitfallFormula
 from .formula_helpers import FormulaHelper
 from cnfgen.clitools import positive_int, positive_even_int
 
-usage_string = """Pitfall Formula
+usage = """usage:
+ {0} <v> <d> <ny> <nz> <k>"""
 
-    The Pitfall formula was designed to be specifically easy for
-    Resolution and hard for common CDCL heuristics. The formula is
-    unsatisfiable and consists of three parts: an easy formula, a hard
-    formula, and a pitfall misleading the solver into working with the
-    hard part.
 
-    The hard part are several copies of an unsatisfiable Tseitin
-    formula on a random regular graph. The pitfall part is made up of
-    a few gadgets over (primarily) two sets of variables: pitfall
-    variables, which point the solver towards the hard part after
-    being assigned, and safety variables, which prevent the gadget
-    from breaking even if a few other variables are assigned.
+description = """The Pitfall formula was designed to be specifically easy for
+Resolution and hard for common CDCL heuristics. The formula is
+unsatisfiable and consists of three parts: an easy formula, a hard
+formula, and a pitfall misleading the solver into working with the
+hard part. For more details, see the corresponding paper by Marc
+Vinyals (AAAI 2020).
 
-    For more details, see the corresponding paper by Marc Vinyals (AAAI 2020).
+example:
+ {0} 45 4 30 5 8      --- parameters used in the paper
 
 positional arguments:
- {0} v,d,ny,nz,k
+  <v>               number of vertices of the Tseitin graph
+  <d>               degree of the Tseitin graph
+  <ny>              number of pitfall variables
+  <nz>              number of safety variables
+  <k>               number of copies of the hard and pitfall parts; controls how
+                    easy the easy part is
 
- where:
- v    --- number of vertices of the Tseitin graph
- d    --- degree of the Tseitin graph
- ny   --- number of pitfall variables
- nz   --- number of safety variables
- k    --- number of copies of the hard and pitfall parts; controls how
-          easy the easy part is
+optional arguments:
+  --help, -h        show this help message and exit
 """
-
-example_string = """example:
- {0} 45 4 30 5 8      --- parameters used in the paper
- """
 
 
 class PitfallCmdHelper(FormulaHelper):
     name = 'pitfall'
-    description = 'Pitfall formula'
 
     @staticmethod
     def setup_command_line(parser):
 
-        parser.usage = usage_string.format(parser.prog)
+        parser.usage = usage.format(parser.prog)
+        parser.description = description.format(parser.prog)
 
-        parser.add_argument('v',
-                            type=positive_int,
-                            help="number of vertices of the Tseitin graph")
-        parser.add_argument('d',
-                            type=positive_int,
-                            help="degree of the Tseitin graph")
-        parser.add_argument('ny',
-                            type=positive_int,
-                            help="number of pitfall variables")
-        parser.add_argument('nz',
-                            type=positive_int,
-                            help="number of safety variables")
-        parser.add_argument(
-            'k',
-            type=positive_even_int,
-            help="number of copies of the hard and pitfall parts")
+        parser.add_argument('v',  type=positive_int)
+        parser.add_argument('d',  type=positive_int)
+        parser.add_argument('ny', type=positive_int)
+        parser.add_argument('nz', type=positive_int)
+        parser.add_argument('k',  type=positive_even_int)
 
     @staticmethod
     def build_cnf(args):
-
         return PitfallFormula(args.v, args.d, args.ny, args.nz, args.k)
